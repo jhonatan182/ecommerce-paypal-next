fea8a03cf27ecaf746e62366c78ee8c1abe7501b
jonavargas-182 <jvargas@redabierta.hn>
2024-12-02 16:37:28 -0600

    agregando pantalla de categorias

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,14 +1,34 @@
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import { Category } from "@/types";
+
 type CategoryByIdPageProps = {
-  params: { id: string };
+  params: { id: Category };
   searchParams: {
     [key: string]: string | string[] | undefined;
   };
 };
 
 export default function CategoryByIdPage({ params }: CategoryByIdPageProps) {
+  const products = initialData.products.filter(
+    (product) => product.gender === params.id
+  );
+
+  const labels: Record<Category, string> = {
+    men: "para hombres",
+    women: "para mujeres",
+    kid: "para niños",
+    unisex: "para todos",
+  };
+
   return (
-    <div>
-      <h1>Hello CategoryByIdPage</h1>
-    </div>
+    <>
+      <Title
+        title={`Artículos ${labels[params.id]}`}
+        subtitle="Todos los productos"
+      />
+
+      <ProductGrid products={products} />
+    </>
   );
 }
